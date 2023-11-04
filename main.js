b9a3cc6cf8dbf5b7b02ff969cc3093ae81f6414c
unknown <damiengurzynski@hotmail.fr>
2023-11-04 18:17:41 +0100

    v0.2

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
 //GLOBAL VARIABLES
 let player=0;
-let dir=1;
+let dir=0;
+let playerview=0;
 
 //OBJECTS
+let map=
+{
+    //variables
+    size: 20,
+
+    //functions
+    init: function()
+    {
+        this.map=Array.from(Array(this.size), ()=>new Array(this.size));
+        for (let i=0;i<this.size;i++)
+            for (let j=0;j<this.size;j++)
+                this.map[i][j]=null;
+    },
+
+    update: function()
+    {
+        //clear
+        for (let i=0;i<this.size;i++)
+        for (let j=0;j<this.size;j++)
+            this.map[i][j]=null;
+
+        //fill with world entities
+        Object.values(world).forEach(e=>
+        {
+            e.forEach(f=>
+            {
+                this.map[f.pos.y][f.pos.x]=f;
+            });
+        });
+    }
+}
+
 let world=
 {
     //static entities
@@ -14,6 +47,10 @@ let world=
 
 let screen=
 {
+    //variables
+    menu: 'main',
+    view: 'front',
+
     //objects
     map:
     {
@@ -56,12 +93,35 @@ let screen=
         screen.ctx.lineTo(canvas.width,canvas.height/2);
         screen.ctx.stroke();
 
-        //draw front of ship
-        screen.ctx.beginPath();
-        screen.ctx.moveTo(0,canvas.height);
-        screen.ctx.lineTo(canvas.width/2,canvas.height/1.3);
-        screen.ctx.lineTo(canvas.width,canvas.height);
-        screen.ctx.stroke();
+        if (screen.view=='front')
+        {
+            //draw front of ship
+            screen.ctx.beginPath();
+            screen.ctx.moveTo(0,canvas.height);
+            screen.ctx.lineTo(canvas.width/2,canvas.height/1.3);
+            screen.ctx.lineTo(canvas.width,canvas.height);
+            screen.ctx.stroke();
+        }
+
+        if (screen.view=='back')
+        {
+            //draw back of ship
+            screen.ctx.beginPath();
+            screen.ctx.moveTo(0,canvas.height);
+            screen.ctx.lineTo(canvas.width/3,canvas.height/1.3);
+            screen.ctx.lineTo(canvas.width/1.5,canvas.height/1.3);
+            screen.ctx.lineTo(canvas.width,canvas.height);
+            screen.ctx.stroke();
+        }
+
+        if (screen.view=='left' || screen.view=='right')
+        {
+            //draw side of ship
+            screen.ctx.beginPath();
+            screen.ctx.moveTo(0,canvas.height/1.3);
+            screen.ctx.lineTo(canvas.width,canvas.height/1.3);
+            screen.ctx.stroke();
+        }
 
         //draw entities
         Object.values(world).forEach(e=>
@@ -74,7 +134,7 @@ let screen=
                     let dy=Math.abs(f.pos.y-p.pos.y);
                     let d=Math.abs(Math.round(Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2))));
                     
-                    if (p.dir=='e' || p.dir=='w')
+                    if (((screen.view=='front' || screen.view=='back') && (p.dir=='e' || p.dir=='w')) || ((p.dir=='n' || p.dir=='s') && (screen.view=='left' || screen.view=='right')))
                     {
                         let minY=p.pos.y-dx;
                         let maxY=p.pos.y+dx;
@@ -95,7 +155,7 @@ let screen=
                             ny=Math.round(screen.canvas.height/2-fsize.y)+(fsize.y/4);
                         }
                         
-                        if (p.dir=='e')
+                        if ((p.dir=='e' && screen.view=='front') || (p.dir=='n' && screen.view=='right') || (p.dir=='s' && screen.view=='left') || (p.dir=='w' && screen.view=='back'))
                         {
                             if (f.pos.x>p.pos.x && f.pos.y>=minY && f.pos.y<=maxY && fsize.y>0)
                             {
@@ -103,7 +163,7 @@ let screen=
                             }
                         }
 
-                        if (p.dir=='w')
+                        if ((p.dir=='w' && screen.view=='front') || (p.dir=='n' && screen.view=='left') || (p.dir=='s' && screen.view=='right') || (p.dir=='e' && screen.view=='back'))
                         {
                             if (f.pos.x<p.pos.x && f.pos.y>=minY && f.pos.y<=maxY && fsize.y>0)
                             {
@@ -112,7 +172,7 @@ let screen=
                         }
                     }
 
-                    if (p.dir=='n' || p.dir=='s')
+                    if (((screen.view=='front' || screen.view=='back') && (p.dir=='n' || p.dir=='s')) || ((p.dir=='e' || p.dir=='w') && (screen.view=='left' || screen.view=='right')))
                     {
                         let minX=p.pos.x-dy;
                         let maxX=p.pos.x+dy;
@@ -133,7 +193,7 @@ let screen=
                             ny=Math.round(screen.canvas.height/2-fsize.y)+(fsize.y/4);
                         }
                         
-                        if (p.dir=='n')
+                        if ((p.dir=='n' && screen.view=='front') || (p.dir=='e' && screen.view=='left') || (p.dir=='w' && screen.view=='right') || (p.dir=='s' && screen.view=='back'))
                         {
                             if (f.pos.y<p.pos.y && f.pos.x>=minX && f.pos.x<=maxX && fsize.y>0)
                             {
@@ -141,7 +201,7 @@ let screen=
                             }
                         }
 
-                        if (p.dir=='s')
+                        if ((p.dir=='s' && screen.view=='front') || (p.dir=='e' && screen.view=='right') || (p.dir=='w' && screen.view=='left') || (p.dir=='n' && screen.view=='back'))
                         {
                             if (f.pos.y>p.pos.y && f.pos.x>=minX && f.pos.x<=maxX && fsize.y>0)
                             {
@@ -155,11 +215,16 @@ let screen=
 
         //update compass
         let c=document.getElementById('compass');
-        if (p.dir=='n') c.innerHTML='&#11032';
-        if (p.dir=='s') c.innerHTML='&#11033';
-        if (p.dir=='w') c.innerHTML='&#11031';
-        if (p.dir=='e') c.innerHTML='&#11030';
+        if (p.dir=='n') c.innerHTML='n<br>w &#11032 e<br>s';
+        if (p.dir=='s') c.innerHTML='n<br>w &#11033 e<br>s';
+        if (p.dir=='w') c.innerHTML='n<br>w &#11031 e<br>s';
+        if (p.dir=='e') c.innerHTML='n<br>w &#11030 e<br>s';
     }
+}
+
+let actions=
+{
+
 }
 
 //CLASSES
@@ -169,7 +234,7 @@ class Island
     {
         this.name=name;
         this.pos=pos;
-        this.size={x:300, y:100};
+        this.size={x:800, y:100};
 
         world.islands.push(this);
     }
@@ -188,71 +253,86 @@ class Ship
     } 
 }
 
-//RUNTIME
-screen.init();
-
-new Ship('Plack Bearl', {x:1,y:5}, 'e');
-new Island('Turtoga', {x:7,y:5});
-new Island('Turtoga', {x:0,y:2});
-
-screen.draw();
-
-
 //KEYBOARD INPUT
 document.addEventListener('keydown',e=>
 {
     let p=world.ships[player];
-
-    //move ship
-    if (p.dir=='n')
-    {
-        if (e.key=='ArrowLeft') world.ships[player].pos.x--;
-        if (e.key=='ArrowRight') world.ships[player].pos.x++;
-        if (e.key=='ArrowUp') world.ships[player].pos.y--;
-        if (e.key=='ArrowDown') world.ships[player].pos.y++;
-    }
-
-    if (p.dir=='s')
-    {
-        if (e.key=='ArrowLeft') world.ships[player].pos.x--;
-        if (e.key=='ArrowRight') world.ships[player].pos.x++;
-        if (e.key=='ArrowUp') world.ships[player].pos.y++;
-        if (e.key=='ArrowDown') world.ships[player].pos.y--;
-    }
-
-    if (p.dir=='e')
-    {
-        if (e.key=='ArrowLeft') world.ships[player].pos.y--;
-        if (e.key=='ArrowRight') world.ships[player].pos.y++;
-        if (e.key=='ArrowUp') world.ships[player].pos.x++;
-        if (e.key=='ArrowDown') world.ships[player].pos.x--;
-    }
-
-    if (p.dir=='w')
-    {
-        if (e.key=='ArrowLeft') world.ships[player].pos.y--;
-        if (e.key=='ArrowRight') world.ships[player].pos.y++;
-        if (e.key=='ArrowUp') world.ships[player].pos.x--;
-        if (e.key=='ArrowDown') world.ships[player].pos.x++;
-    }
-
-    //rotate ship
     let rdir=['n','e','s','w'];
-
-    if (e.key=='v')
+    let rview=['front','right','back','left'];
+    
+    if (screen.menu='main')
     {
-        if (dir<rdir.length-1) dir++;
-        else dir=0;
-        p.dir=rdir[dir];
+        //change view
+        if (e.key==' ')
+        {
+            if (playerview<rview.length-1) playerview++;
+            else playerview=0;
+            screen.view=rview[playerview];
+        }
+
+        //ship linear movement
+        if (e.key=='ArrowLeft')
+        {
+            if (dir>0) dir--;
+            else dir=rdir.length-1;
+            p.dir=rdir[dir];
+        }
+        if (e.key=='ArrowRight')
+        {
+            if (dir<rdir.length-1) dir++;
+            else dir=0;
+            p.dir=rdir[dir];
+        }
+        if (e.key=='ArrowUp')
+        {
+            if (p.dir=='n' && p.pos.y>0 && map.map[p.pos.y-1][p.pos.x]==null)
+                p.pos.y--;
+            if (p.dir=='s' && p.pos.y<map.size-1 && map.map[p.pos.y+1][p.pos.x]==null)
+                p.pos.y++;
+            if (p.dir=='e' && p.pos.x<map.size-1 && map.map[p.pos.y][p.pos.x+1]==null)
+                p.pos.x++;
+            if (p.dir=='w' && p.pos.x>0 && map.map[p.pos.y][p.pos.x-1]==null)
+                p.pos.x--;
+        }
+        if (e.key=='ArrowDown')
+        {
+            if (p.dir=='n' && p.pos.y<map.size-1 && map.map[p.pos.y+1][p.pos.x]==null)
+                p.pos.y++;
+            if (p.dir=='s' && p.pos.y>0 && map.map[p.pos.y-1][p.pos.x]==null)
+                p.pos.y--;
+            if (p.dir=='e' && p.pos.x>0 && map.map[p.pos.y][p.pos.x-1]==null)
+                p.pos.x--;
+            if (p.dir=='w' && p.pos.x<map.size-1 && map.map[p.pos.y][p.pos.x+1]==null)
+                p.pos.x++;
+        }
+
+        //ship rotation
+        if (e.key=='c')
+        {
+            if (dir>0) dir--;
+            else dir=rdir.length-1;
+            p.dir=rdir[dir];
+        }
+        if (e.key=='v')
+        {
+            if (dir<rdir.length-1) dir++;
+            else dir=0;
+            p.dir=rdir[dir];
+        }
     }
 
-    if (e.key=='c')
-    {
-        if (dir>0) dir--;
-        else dir=rdir.length-1;
-        p.dir=rdir[dir];
-    }
-
-    //update screen
+    //update
+    map.update();
     screen.draw();
 })
+
+//RUNTIME
+screen.init();
+map.init();
+
+new Ship('Plack Bearl', {x:1,y:5}, 'n');
+new Island('Turtoga', {x:7,y:5});
+new Island('Skull', {x:0,y:2});
+
+screen.draw();
+map.update();
